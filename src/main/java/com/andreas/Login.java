d2bb97b4f71dc0f7e50d9e8c716087b8ab41de62
AndreasSjo <andreas.sjoden@hotmail.com>
2022-01-04 16:04:55 +0100

    Cleaned up code

--- src/main/java/com/andreas/Login.java
+++ src/main/java/com/andreas/Login.java
@@ -1,4 +1,5 @@
 package com.andreas;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -6,121 +7,113 @@ import java.sql.*;
 import javax.swing.*;
 
 public class Login {
-    private String username,password;
+    private String username, password;
+    private boolean loginSuccess = false;
+    JPanel panel = new JPanel();
+    JFrame frame = new JFrame("Login");
+    GridBagConstraints gbc = new GridBagConstraints();
+    GridBagLayout gbl = new GridBagLayout();
+    Insets inset = new Insets(10, 10, 10, 10);
 
-    public Login(){
-        JPanel panel = new JPanel();
-        JFrame frame = new JFrame("Login");
+    public Login() {
         frame.setSize(350, 350);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
 
-        panel.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
+        panel.setLayout(gbl);
 
-        JLabel label = new JLabel("User");
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.insets = new Insets(0,20,0,0);
-        c.gridx = 0;
-        c.gridy = 0;
-        panel.add(label, c);
+        JLabel userLabel = new JLabel("User");
 
-        JTextField userText = new JTextField("", 20);
-        c.fill = c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.insets = new Insets(0,0,0,20);
-        c.gridx = 1;
-        c.gridy = 0;
-        panel.add(userText, c);
+
+        JTextField userField = new JTextField("", 20);
+
 
         JLabel passwordLabel = new JLabel("Password");
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.insets = new Insets(20,20,0,20);
-        c.gridx = 0;
-        c.gridy = 1;
-        panel.add(passwordLabel, c);
 
-        
+
         JPasswordField passwordField = new JPasswordField("", 20);
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.insets = new Insets(20,20,0,20);
-        c.gridx = 1;
-        c.gridy = 1;
-        panel.add(passwordField, c);
+
 
         JButton loginButton = new JButton("Login");
-            loginButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                username = userText.getText();
-                // Insecure use of password.. Change later
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                username = userField.getText();
+                // TODO how to secure password?
                 password = String.valueOf(passwordField.getPassword());
-                System.out.println(password);
-                tryLogin(username,password);
-                
-                }
-            });
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.insets = new Insets(20,20,0,20);
-        c.gridx = 0;
-        c.gridy = 2;
-        panel.add(loginButton, c);
 
-        
+                if (tryLogin(username, password) == true) {
+                    Index i = new Index(username);
+                    frame.dispose();
+                }
+
+            }
+        });
+
+
         JButton cancelButton = new JButton("Cancel");
-            cancelButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                  System.exit(0);
-                
-                }
-            });
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.insets = new Insets(20,20,0,20);
-        c.gridx = 1;
-        c.gridy = 2;
-        panel.add(cancelButton, c);
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
 
+            }
+        });
+
+
+        addComponent(userLabel, 1, 0, 1, 1, inset);
+        addComponent(userField, 1, 1, 1, 1, inset);
+        addComponent(passwordLabel, 2, 0, 1, 1, inset);
+        addComponent(passwordField, 2, 1, 1, 1, inset);
+        addComponent(loginButton, 3, 0, 1, 1, inset);
+        addComponent(cancelButton, 3, 1, 1, 1, inset);
         frame.setVisible(true);
 
     }
-    public void tryLogin(String username, String password){
-        try{  
-            Class.forName("com.mysql.cj.jdbc.Driver");  
-            Connection con=DriverManager.getConnection(  
-            "jdbc:mysql://localhost:3306/bankdb","user","password");  
-            //bankdb is the database name, user is username and password is...
-            //in my docker image. Change to whatever you have  
-            Statement stmt=con.createStatement();  
-            ResultSet rs=stmt.executeQuery("SELECT id FROM user WHERE userName LIKE '" +
-                                             username +"'");  
-            
-            if(rs.next()){  
-                ResultSet rs2=stmt.executeQuery(
-                    "SELECT id FROM user WHERE userName LIKE '" +
-                    username +"'" +
-                    " AND password LIKE '" +
-                    password.toString() + "'");
-                        if(rs2.next()){
-                            JOptionPane.showMessageDialog(null, "Login successful");
-                            con.close();
-                        }
-                        else if(rs2.next() == false){
-                            JOptionPane.showMessageDialog(null, "Incorrect password");
-                        }
-                }
-            else if(rs.next() == false){
-                JOptionPane.showMessageDialog(null, "User does not exist");
-            }
-            }catch(Exception e){
-                 System.out.println(e);
-                }  
-            }  
+
+    private void addComponent(Component component, int y,
+            int x, int width, int height, Insets inset) {
+
+        gbc.gridy = y;
+        gbc.gridx = x;
+        gbc.gridwidth = width;
+        gbc.gridheight = height;
+        gbc.insets = inset;
+        gbl.setConstraints(component, gbc);
+        panel.add(component);
     }
 
+    public boolean tryLogin(String username, String password) {
+        boolean loginOk = false;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/bankdb", "user", "password");
+            // bankdb is the database name, user is username and password is...
+            // in my docker image. Change to whatever you have
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT id FROM user WHERE userName LIKE '" +
+                    username + "'");
 
+            if (rs.next()) {
+                ResultSet rs2 = stmt.executeQuery(
+                        "SELECT id FROM user WHERE userName LIKE '" +
+                                username + "'" +
+                                " AND password LIKE '" +
+                                password.toString() + "'");
+                if (rs2.next()) {
+                    JOptionPane.showMessageDialog(null, "Login successful");
+                    loginOk = true;
+                    con.close();
+                } else if (rs2.next() == false) {
+                    JOptionPane.showMessageDialog(null, "Incorrect password");
+                }
+            } else if (rs.next() == false) {
+                JOptionPane.showMessageDialog(null, "User does not exist");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return loginOk;
+    }
+}
